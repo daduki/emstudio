6d87a101b66c417691a8726df1acdb1fb88c86c9
Daeduek Cho <daduki@gmail.com>
2023-08-30 15:37:14 +0900

    [common] 0.31 firebase settings

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDNZpNmmgKUNQwq5P6qu7tx05LDWGhrMjA",
-  authDomain: "emstudio-ed70a.firebaseapp.com",
-  projectId: "emstudio-ed70a",
-  storageBucket: "emstudio-ed70a.appspot.com",
-  messagingSenderId: "1065642594309",
-  appId: "1:1065642594309:web:ecf1054a73c7f68816cc6c"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_ID,
 };
 
 export default firebase.initializeApp(firebaseConfig);
